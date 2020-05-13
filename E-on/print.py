6892f026c13cb41cb8cc559fc649df0c61b2d9d7
beapilot69 <64725311+beapilot69@users.noreply.github.com>
2020-05-14 04:37:28 +0900

    Update print.py

--- E-on/print.py
+++ E-on/print.py
@@ -5,11 +5,11 @@ priorities2 = []
 for i in range(n):
     priorities2.append(int(i))
 
-priorities2[m] = m  #구할 작업 번호의 위치를 priorities_ 리스트의 같은 위치에 1로 설정
+priorities2[m] = m  #단순 연산만을 위한 리스트
 time = 0    #출력시간 초기값 지정
 
 while True:
-    if priorities[0] == max(priorities):    #처음에 9라 했다가 가장 큰 우선순위가 무엇인지 모를 수 있으므로 max사용.
+    if priorities[0] == max(priorities):  #가장 큰 우선순위가 무엇인지 모를 수 있으므로 max사용.
         time += 1       #첨부터 가장 큰 우선순위면 바로 출력이므로 출력시간 = +1분
         if priorities2[0] == m:
             print(time)

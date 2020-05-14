9fe3b0ec11a5244410e928c12a0af93a3d679a22
beapilot69 <64725311+beapilot69@users.noreply.github.com>
2020-05-14 11:23:24 +0900

    Update print.py

--- E-on/print.py
+++ E-on/print.py
@@ -6,11 +6,11 @@ for i in range(n):
     priorities2.append(int(i))
 
 priorities2[m] = m  #단순 연산만을 위한 리스트
-time = 0    #출력시간 초기값 지정
+time = 0 
 
 while True:
-    if priorities[0] == max(priorities):  #가장 큰 우선순위가 무엇인지 모를 수 있으므로 max사용.
-        time += 1       #첨부터 가장 큰 우선순위면 바로 출력이므로 출력시간 = +1분
+    if priorities[0] == max(priorities):
+        time += 1
         if priorities2[0] == m:
             print(time)
             break
